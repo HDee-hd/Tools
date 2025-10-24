8735ce4bada314d1588eaeaa4942d6ff7fb08c48
Hassan Dairo <83604257+HDee-hd@users.noreply.github.com>
2025-10-24 16:37:09 +0100

    Add files via upload

---
+++ healthInfo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+######################
+# Author: HDairo
+# Dairo: 24/10/2025
+#
+# This is a script that outputs the node's health
+#
+# Version: V1
+# #####################
+
+
+set -x #Debug mode
+
+df -h
+
+free -g
+
+nproc
+
+
