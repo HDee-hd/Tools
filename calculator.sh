be2cc6cbe971392fd68652de633cbf7ef3bf28fa
Hassan <hdairo199@gmail.com>
2025-10-29 17:41:19 +0100

    Subtraction function

---
+++ calculator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+###################################
+#
+# Author: HDairo
+#
+#
+###################################
+
+add x + y
+
+# Subtraction function
+subtract() {
+    echo "Result: $((x - y))"
+}
